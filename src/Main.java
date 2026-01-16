8518d9171d43bc0200a047c66232e1829dd0ec23
Dilara Dinç <dilaradinc.0009@gmail.com>
2026-01-16 17:49:49 +0300

    Use sorting and searching algorithms in main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import searching.LinearSearch;
+import sorting.BubbleSort;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Java Algorithms Practice");
+
+        int[] numbers = {5, 2, 9, 1, 3};
+
+        BubbleSort.sort(numbers);
+
+        System.out.print("Sorted array: ");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+
+        int index = LinearSearch.search(numbers, 9);
+        System.out.println("\nIndex of 9: " + index);
     }
 }
